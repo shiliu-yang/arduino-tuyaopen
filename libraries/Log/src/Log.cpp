4c9aa318e1e70e889ee3e183023514df75d4a446
YangJie <shiliu.yang@tuya.com>
2024-08-12 11:46:48 +0800

    Update

--- libraries/Log/src/Log.cpp
+++ libraries/Log/src/Log.cpp
@@ -23,9 +23,19 @@ void LogClass::setLevel(LogLevel level)
   tal_log_set_level(static_cast<TAL_LOG_LEVEL_E>(level));
 }
 
-void LogClass::setColor(bool enable)
+void LogClass::enableMsInfo(bool enable)
+{
+  tal_log_set_ms_info(enable);
+}
+
+void LogClass::enableColor(bool enable)
 {
   tal_log_color_enable_set(enable);
+}
+
+void LogClass::setColor(LogLevel level, ColorMode mode, ColorFont font, ColorBG background)
+{
+  tal_log_color_set(static_cast<TAL_LOG_LEVEL_E>(level), static_cast<TAL_LOG_DISPLAY_MODE_E>(mode), static_cast<TAL_LOG_FONT_COLOR_E>(font), static_cast<TAL_LOG_BACKGROUND_COLOR_E>(background));
 }
 
 LogClass Log;
--- libraries/Log/src/Log.h
+++ libraries/Log/src/Log.h
@@ -10,17 +10,32 @@ namespace arduino {
 
 #define OPEN_SDK_LOG_BUFFER_SIZE 1024
 
+
+
 class LogClass {
 public:
   enum LogLevel {
     ERROR, WARN, NOTICE, INFO, DEBUG, TRACE
+  };
+  enum ColorMode {
+    MODE_DEFAULT = 0, MODE_HIGH_LIGHT, MODE_UNDER_LINE = 4, MODE_FLASH = 5, MODE_REVERSE = 7
+  };
+  enum ColorFont {
+    FONT_BLACK = 30, FONT_RED, FONT_GREEN, FONT_YELLOW, FONT_BLUE, FONT_PURPLE, FONT_CYAN, FONT_WHITE, FONT_DEFAULT = 39
+  };
+  enum ColorBG { // background color
+    BG_BLACK = 40, BG_RED, BG_GREEN, BG_YELLOW, BG_BLUE, BG_PURPLE, BG_CYAN, BG_WHITE, BG_DEFAULT = 49
   };
 
   LogClass();
   ~LogClass();
 
   void setLevel(LogLevel level);
-  void setColor(bool enable);
+
+  void enableMsInfo(bool enable);
+
+  void enableColor(bool enable);
+  void setColor(LogLevel level, ColorMode mode, ColorFont font, ColorBG background);
 };
 
 } // namespace arduino
