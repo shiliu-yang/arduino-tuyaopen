6452f55b4291c2f44715dcfb904c3c711099f111
YangJie <shiliu.yang@tuya.com>
2024-10-09 10:09:41 +0800

    Update

--- cores/tuya_open/wiring.cpp
+++ cores/tuya_open/wiring.cpp
@@ -2,11 +2,6 @@
 
 #include "tal_system.h"
 
-#if defined(ARDUINO_TUYA_T2)
-#include "FreeRTOS.h"
-#include "portmacro.h"
-#endif // defined(ARDUINO_TUYA_T2)
-
 unsigned long millis()
 {
     unsigned long ms = tal_system_get_millisecond();
@@ -30,8 +25,5 @@ void delayMicroseconds(unsigned int us)
 
 void yield(void)
 {
-#if defined(ARDUINO_TUYA_T2)
-    portYIELD();
-#endif // defined(ARDUINO_TUYA_T2)
-    return;
+    tal_system_sleep(1);
 }
